6a4ceb608b710a80b212fbc92779117d33be4298
ssa <webdev2027@gmail.com>
2020-07-16 14:06:09 +0200

    edit syntax error

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:hellotypo3-htidelements/Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts.tsconfig">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:hellotypo3-gridelements/Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts.tsconfig">'
 );
